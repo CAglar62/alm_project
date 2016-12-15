d0e14e380412927e20f99a7ac4e178ee3b5bb34b
CAglar62 <caglar.aglar@ug.bilkent.edu.tr>
2016-12-15 19:09:59 +0200

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,10 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            /////////////
-               ////
-            ///
-            double a;
+        
             Program p1 = new ConsoleApplication2.Program(); // deneme 
             String yourname = "Mr. Nice";                       // deneme 2
             Console.WriteLine("Hello {0}", yourname);       //deneme 3
